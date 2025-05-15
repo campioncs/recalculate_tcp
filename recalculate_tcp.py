3a6d5fb63b9512fda1a9a56f7d08afa5952af4d9
Caleb <campioncs@gmail.com>
2025-05-14 20:53:02 -0400

    Added coments to recalculate_tcp.py

--- recalculate_tcp.py
+++ recalculate_tcp.py
@@ -1,6 +1,9 @@
+#Launches recalculate interface
 from tkinter import Tk
 from gui import GuiTcp
-
+#Create root window
 root = Tk()
+#Pass root window to GuiTcp class
 GuiTcp(root)
+#Start event loop
 root.mainloop()
